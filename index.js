5d73f5c405db7d57912c49a54306bade27624056
Admin <admin@MacBook-Air-von-Admin.local>
2023-07-15 13:38:03 +0200

    added cors

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors=require("cors");
+
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
@@ -26,6 +28,7 @@ const partners = require('./routes/partners');
 // const mainRoute = require('./routes/index');
 
 const app = express();
+app.use(cors())
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
